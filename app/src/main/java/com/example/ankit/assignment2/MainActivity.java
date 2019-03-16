4a957d6acf09722f8c4222c2851c27b452535150
atibrewal98 <ankit@siplnet.com>
2019-03-17 05:00:54 +0800

    Create Layout Design

--- app/src/main/java/com/example/ankit/assignment2/MainActivity.java
+++ app/src/main/java/com/example/ankit/assignment2/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.ankit.assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton[] cards;
+
+    private void initCardImage() {
+        for (int i = 0; i < 4; i++) {
+            int resID = getResources().getIdentifier("back_0","drawable","com.example.ankit.assignment2");
+                    cards[i].setImageResource(resID);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cards = new ImageButton[4];
+        cards[0] = (ImageButton) findViewById(R.id.card1);
+        cards[1] = (ImageButton) findViewById(R.id.card2);
+        cards[2] = (ImageButton) findViewById(R.id.card3);
+        cards[3] = (ImageButton) findViewById(R.id.card4);
         setContentView(R.layout.activity_main);
     }
 }
